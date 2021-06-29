0f3367941121830cc6b65492233e89f51b7df6bc
Puma Prince <christopherbreen22@gmail.com>
2021-06-28 20:58:44 -0400

    created function so that what is typed in will actually show up

--- script.js
+++ script.js
@@ -2,23 +2,42 @@ var saveAct = $('#saveAct');
 var forminput = $('#formEnter');
 var activityList = $('#activityList');
 var printedAct = $('#toDo');
+var saveBttn = $('.Save');
+
+
+saveBttn.on('click',function(event){
+    event.preventDefault();
+    //console.log("");
+    var usertText = $(this).siblings('input').val();
+    var userHour = $(this).siblings('input').attr("id");
+    localStorage.setItem(userHour,usertText);
+    renderActivity();
+})
+
+
+
+
+
 //this function apends the list in the html with what is entered 
-var printActivity = function(form){
-    var listEl =$('<li>');
+var printActivity = function(activity,i){
+   var listEl =$('<li>');
+   listEl.text(activity);
     listEl.addClass('activityItem');
-    listEl.appendTo(activityList);
+    //listEl.appendTo
+    $("#activityList"+ i).html(listEl);
+    
 };
 
-var handleFormsubmit = function(event){
-    event.preventDefault();
 
-    var activityInput = forminput.val();
-    
-
-
-    printActivity(activityInput);
+var renderActivity = function(){
+    printActivity(localStorage.getItem("8"),8)
+    printActivity(localStorage.getItem("9"),9)
+    printActivity(localStorage.getItem("10"),10)
 }
+renderActivity();
 
-printedAct.on('submit', handleFormsubmit);
 
 
+//var colorchange = function(){
+  //  if 
+//}
